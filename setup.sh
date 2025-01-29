7aa237af516c7ee468932f165b3fd18e2336f0de
Manuel Alejandro Chaves Herre <manuelchavesherrera69@gmail.com>
2025-01-29 10:52:15 +0100

    ultima

--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-# Verifica si existe el archivo descargado
+# URL de tu repositorio y la ID de la ejecución de GitHub Actions
+GITHUB_REPO="mmanuch/test-secrets"  # Reemplázalo con tu nombre de usuario y repo
+GITHUB_RUN_ID="13028490273"  # Actualiza con la ID de la ejecución de GitHub
+
+echo "🔽 Descargando .env desde GitHub Artifacts..."
+gh run download $GITHUB_RUN_ID -R $GITHUB_REPO -n env-file
+
+# Verifica si el archivo .env se descargó
 if [ -f "./env-file/.env" ]; then
     mv ./env-file/.env .env
-    echo ".env movido al proyecto ✅"
+    echo "✅ .env movido al proyecto"
 else
-    echo "❌ No se encontró el archivo .env"
+    echo "⚠️ No se encontró el archivo .env, descárgalo manualmente desde GitHub."
 fi
